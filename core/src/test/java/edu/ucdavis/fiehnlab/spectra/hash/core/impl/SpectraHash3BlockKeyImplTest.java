d4b82b1a0d3835d335c0ebcf4c2ed6ee528d2bd4
Gert <wohlgemuth@ucdavis.edu>
2015-07-03 12:40:45 -0700

    remducing reduntant tests

--- core/src/test/java/edu/ucdavis/fiehnlab/spectra/hash/core/impl/SpectraHash3BlockKeyImplTest.java
+++ core/src/test/java/edu/ucdavis/fiehnlab/spectra/hash/core/impl/SpectraHash3BlockKeyImplTest.java
@@ -16,35 +16,6 @@ import static org.junit.Assert.*;
  */
 public class SpectraHash3BlockKeyImplTest extends AbstractSpectraHashImplTester {
 
-    @Test
-    public void testGenerate() throws Exception {
-
-
-        SpectraHash impl = new SpectraHash3BlockKeyImpl();
-
-        impl.addListener(new HashingListener() {
-            public void eventReceived(HashingEvent e) {
-                System.out.println(e.getBlock() + ":" + e.getRawValue());
-                System.out.println(e.getBlock() + ":" + e.getHashedValue());
-            }
-
-            public void hashingComplete(Spectrum spectrum, String hash) {
-
-            }
-
-        });
-
-        Spectrum spectrum = new SpectrumImpl(Arrays.asList(new Ion(100, 1), new Ion(101, 2), new Ion(102, 3)),"mona");
-
-        String hash = impl.generate(spectrum);
-
-        System.out.println(hash);
-        System.out.println(hash.length());
-
-        assertEquals("5d20a4d7ee6c49d52f70734bae8bae0f1bab67a4-16840fdada28160115bf616ff1bf716ef9eba88a83ebc259c2fc9a601bb66f14-0",hash);
-
-    }
-
     @Override
     SpectraHash getHashImpl() {
         return new SpectraHash3BlockKeyImpl();

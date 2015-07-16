9f8944711368b65a495dc8c85b3374344cc05694
Diego Pedrosa <linuxmant@gmail.com>
2015-07-16 15:01:34 -0700

    add csharp implementation

---
+++ csharp/splash/Spectrum.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace NSSplash {
+	
+	public sealed class Spectrum {
+		private SpectrumType type;
+		public SpectrumType Type { 
+			get { return type; }
+			set { type = value; }
+		}
+
+		private List<Ion> ions = new List<Ion>();
+		public List<Ion> Ions {
+			get { return ions; }
+			set { ions = value; }
+		}
+
+		public Spectrum (string data, SpectrumType type) {
+			//checke data has data 
+			if ("" == data) {
+				throw new ArgumentException ("The spectrum data can't be null or empty.");
+			}
+
+			this.type = type;
+
+			ions = new List<Ion>();
+			Array splitData = data.Split (' ');
+			foreach(string ion in splitData) {
+				//get m/z
+				double mz = Double.Parse(String.Format("{0:F6}", ion.Split(':')[0]));
+
+				//get intensity
+				double intensity = Double.Parse(String.Format("{0:F6}", ion.Split(':')[1]));
+
+				Ion newIon = new Ion(mz, intensity);
+				Ions.Add(newIon);
+			}
+
+			Ions = this.normalizeIons();
+		}
+
+		public override string ToString() {
+			StringBuilder ionList = new StringBuilder();
+			foreach(Ion ion in Ions) {
+				ionList.Append(ion);
+				ionList.Append(' ');
+			}
+
+			if(ionList.Length > 1) {
+				ionList.Remove(ionList.Length - 1, 1);
+			}
+
+			return string.Format("[Spectrum: Type={0}, Ions={1}]", Type, ionList.ToString());
+		}
+
+		public List<Ion> getIonsByIntensity(bool asc = false) {
+			List<Ion> sorted = Ions.OrderByDescending(i => i.Intensity).ThenBy(m => m.MZ).ToList();
+
+			if(asc) { 
+				sorted.Reverse();
+			}
+
+			return sorted;
+		}
+
+		public List<Ion> getIonsByMZ(bool desc = false) {
+			List<Ion> sorted = Ions.OrderBy(i => i.MZ).ThenByDescending(i => i.Intensity).ToList();
+
+			if(desc) {
+				sorted.Reverse();
+			}
+
+			return sorted;
+		}
+
+		private List<Ion> normalizeIons() {
+			double maxInt = Ions.Max(ion => ion.Intensity);
+			Ions.ForEach(i => i.Intensity = i.Intensity / maxInt * 1000);
+
+			return Ions;
+		}
+	}
+}
+
---
+++ csharp/splash/Splash.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Linq;
+using System.Numerics;
+
+namespace NSSplash {
+	public class Splash : ISplash {
+		private const string PREFIX = "splash";
+		private const int VERSION = 0;
+		private const int FACTOR = 1000000;
+
+
+		public string splashIt(Spectrum spectrum) {
+
+			// check spectrum var
+			if (spectrum == null) {
+				throw new ArgumentNullException ("The spectrum can't be null");
+			}
+
+			StringBuilder hash = new StringBuilder();
+
+			//creating first block 'splash<type><version>'
+			hash.Append(getFirstBlock(spectrum.Type));
+			hash.Append('-');
+
+			//creating second block 'top 10 peak hash' (SHA256)
+			hash.Append(getTop10Hash(spectrum));
+			hash.Append('-');
+
+			//create the 3rd block
+			hash.Append(getSpectrumBlock(spectrum));
+			hash.Append('-');
+
+			//create version block
+			hash.Append(getSumBlock(spectrum));
+
+			return hash.ToString();
+
+		}
+
+		private string getFirstBlock(SpectrumType specType) {
+			return(PREFIX + (int)specType + VERSION);
+		}
+
+		private string getTop10Hash(Spectrum spec) {
+			List<Ion> ions = spec.getIonsByIntensity();
+
+			if(ions.Count > 10) {
+				ions.RemoveRange(10, ions.Count - 10);
+			}
+
+			StringBuilder strIons = new StringBuilder();
+			foreach(Ion i in ions) {
+				strIons.Append(formatNumber(i.MZ));
+				strIons.Append(" ");
+			}
+
+
+			//string to hash
+			strIons.Remove(strIons.Length -1, 1);
+
+			byte[] message = Encoding.UTF8.GetBytes(strIons.ToString());
+
+			SHA256Managed hashString = new SHA256Managed();
+
+			hashString.ComputeHash(message);
+
+			string hash = BitConverter.ToString(hashString.Hash).Replace("-","").ToLower();
+			hash = hash.Replace("-","").Substring(0,10);
+
+//			Console.WriteLine("2nd block raw: {0}\n2rd block pro: {1}", strIons, hash);
+
+			return hash;
+		}
+
+
+		//calculate the hash for the whole spectrum
+		private string getSpectrumBlock(Spectrum spec) {
+			List<Ion> ions = spec.getIonsByMZ();
+
+			StringBuilder strIons = new StringBuilder();
+			foreach(Ion i in ions) {
+				strIons.Append(String.Format("{0}:{1}", formatNumber(i.MZ), formatNumber(i.Intensity)));
+				strIons.Append(" ");
+			}
+
+			//string to hash
+			strIons.Remove(strIons.Length -1, 1);
+
+			byte[] message = Encoding.UTF8.GetBytes(strIons.ToString());
+
+			SHA256Managed hashString = new SHA256Managed();
+
+			hashString.ComputeHash(message);
+
+			string hash = BitConverter.ToString(hashString.Hash);
+			hash = hash.Replace("-","").Substring(0,20).ToLower();
+
+//			Console.WriteLine("3nd block raw: {0}\n3rd block pro: {1}", strIons, hash);
+
+			return hash;
+		}
+
+		//calculate Sum(mz*intensity) for top 100 ions (sorted by intensity desc)
+		private string getSumBlock(Spectrum spec) {
+			BigInteger bisum = 0;
+			List<Ion> ions = spec.getIonsByIntensity();
+
+			if(ions.Count > 100) {
+				ions.RemoveRange(100, ions.Count - 100);
+			}
+
+			foreach(Ion i in ions) {
+				bisum = BigInteger.Add(bisum, BigInteger.Multiply(new BigInteger(i.MZ * FACTOR), new BigInteger(i.Intensity * FACTOR)));
+			}
+
+			long sum = (long)BigInteger.Divide(bisum, new BigInteger(1000000000000));
+
+//			Console.WriteLine(String.Format("Sum: {0}", sum));
+
+			return String.Format("{0:D10}", (long)sum);
+		}
+
+		private string formatNumber(double number) {
+			return String.Format("{0}", (long)(number * FACTOR));
+		}
+	}
+}
+

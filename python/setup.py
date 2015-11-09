228075458afc5bf1b06cebf86e7d04b738c07ec4
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2015-11-09 15:44:41 -0800

    restructure python version and add install script

---
+++ python/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+
+setup(name = 'Splash (Spectrum Hash)',
+      version = '1.0',
+      description = 'Python implementation of the SPLASH spectrum hash algorithm',
+      author='Sajjan Singh Mehta',
+      author_email = 'sajjan.s.mehta@gmail.com',
+      url = 'https://splash.fiehnlab.ucdavis.edu',
+      packages = ['splash'],
+)

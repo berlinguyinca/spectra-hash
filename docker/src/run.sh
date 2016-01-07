530711d9f6612e23044ab44b6281b01641fc179a
Gert Wohlgemuth <wohlgemuth@ucdavis.edu>
2016-01-07 15:11:53 -0800

    updated docker file and added tracking of usage, utilizing awstats

--- docker/src/run.sh
+++ docker/src/run.sh
@@ -1,3 +1,21 @@
 #!/usr/bin/env bash
+
+
+#includes lsb functions
+. /lib/lsb/init-functions
+
+# Start cron for needed regular logrotate
+CRON=`which cron`
+log_daemon_msg "Starting ${CRON}"
+$CRON
+
+log_daemon_msg "starting nginx"
+
+service nginx start
+
+log_daemon_msg "starting logrotate"
+logrotate -f /etc/logrotate.conf
+
+log_daemon_msg "starting splash app"
 cd /opt
 java -Xmx512m -jar splash.jar

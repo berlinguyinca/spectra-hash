d7ebf77914ed88b63347817f2cd2dde1c9864279
Benjamin Nguyen <bnguyen@fry.genomecenter.ucdavis.edu>
2016-01-14 13:00:29 -0800

    completed splash key and validation REST API testing

--- javascript/splash.js
+++ javascript/splash.js
@@ -9,81 +9,87 @@ var SPLASH_VALIDATE = 'http://splash.fiehnlab.ucdavis.edu/splash/validate';
 
 
 function formatData(spectra) {
-    if (typeof spectra === 'object') {
-        return JSON.stringify(spectra);
-    }
-    else {
-        console.log('Spectra must be an Object');
-        return 'undefined';
-    }
+	if (typeof spectra === 'object') {
+		return JSON.stringify(spectra);
+	}
+	else {
+		console.log('Spectra must be an Object');
+		return 'undefined';
+	}
 }
 
 /**
  * loads spectra to REST service and retrieve the splash key.
- * @param spectra
+ * @param spectra Object {}
  * @return splashKey
  */
 function loadSplash(spectra) {
 
-    var splashKey = '';
+	var splashKey = '';
 
-    // verify spectra object
-    var serializeSpectra = formatData(spectra);
+	// verify spectra object
+	var serializeSpectra = formatData(spectra);
 
-    // make ajax call
-    if (serializeSpectra !== 'undefined') {
-        $.ajax({
-            type: 'POST',
-            contentType: 'application/json',
-            dataType: 'json',
-            url: SPLASH_REST,
-            data: serializeSpectra,
-            success: function (data) {
-                splashKey = data;
-                console.log(splashKey);
-            },
-            error: function (error, msg, httpStatus) {
-                console.log('ERROR ' + msg + ' STATUS ' + httpStatus);
-            }
-        });
-    }
+	// make ajax call
+	if (serializeSpectra !== 'undefined') {
+		$.ajax({
+			type: 'POST',
+			contentType: 'application/json; charset=utf-8',
+			url: SPLASH_REST,
+			data: serializeSpectra,
+			success: function (data) {
+				splashKey = data;
+			},
+			error: function (xhr, status, errorThrown) {
+				console.log('XHR ' + JSON.stringify(xhr));
+				console.log('STATUS ' + status);
+				console.log('ERROR THROWN ' + errorThrown);
+			}
+		});
+	}
 
-    return splashKey;
+	return splashKey;
 }
 
 
 /**
  * loads a spectra object against our REST service to validate the splash key.
- * @param spectra
+ * @param spectra Object {}
  * @return returns JSON object
  */
 function validateSplash(spectra) {
 
-    var result = {};
+	var result = {};
 
-    // validate spectra object
-    if (typeof spectra !== 'object') {
-        alert('Spectra must be a valid JSON object');
-    }
-    else if (typeof spectra.splash === 'undefined') {
-        alert('request must have a valid splash key');
-    }
+	// validate spectra object
+	if (typeof spectra !== 'object') {
+		return console.log('Spectra must be a valid JSON object');
+	}
+	else if (typeof spectra.splash === 'undefined') {
+		return console.log('request must have a valid splash key');
 
-    // make ajax call
-    else {
-        $.ajax({
-            type: 'POST',
-            contentType: 'application/json',
-            dataType: 'json',
-            url: SPLASH_VALIDATE,
-            data: spectra,
-            success: function (data) {
-                result = data;
-            },
-            error: function (error, msg, httpStatus) {
-                console.log('ERROR ' + msg + ' STATUS ' + httpStatus);
-            }
-        });
-    }
-    return result;
+	}
+
+	// make ajax call
+	else {
+		var serializeSpectra = formatData(spectra);
+
+		if (serializeSpectra !== 'undefined') {
+			$.ajax({
+				type: 'POST',
+				contentType: 'application/json; charset=utf-8',
+				url: SPLASH_VALIDATE,
+				data: serializeSpectra,
+				success: function (data) {
+					result = data;
+				},
+				error: function (xhr, status, errorThrown) {
+					console.log('XHR ' + JSON.stringify(xhr));
+					console.log('STATUS ' + status);
+					console.log('ERROR THROWN ' + errorThrown);
+				}
+			});
+		}
+	}
+	return result;
 }

205b1f699d149630426fa393254c6f109693a0ea
Diego Pedrosa <linuxmant@gmail.com>
2026-02-24 11:41:12 -0800

    updated Dockerfile base image to eclipse-temurin:8-jre-focal, improved run.sh script robustness, added configurable docker tags, and streamlined image build process

--- docker/build.sh
+++ docker/build.sh
@@ -4,18 +4,25 @@ set -o pipefail
 NAME="splash/web"
 IMAGE="eros.fiehnlab.ucdavis.edu/$NAME"
 
+if [ -z "${2:-}" ]; then
+  TAG="latest"
+else
+  TAG="$2"
+fi
+
+
 cd ../docker
 pwd
 docker build -t ${IMAGE} --iidfile build.log --rm=true . || exit 1
 ID=$(tail -1 build.log | awk -F: '{print $2;}')
-echo "tagging: $ID ${IMAGE}:wcag"
-docker tag $ID ${IMAGE}:wcag
-echo "tagging: $ID ${NAME}:wcag"
-docker tag $ID ${NAME}:wcag
+echo "tagging: $ID ${IMAGE}:$TAG"
+docker tag $ID ${IMAGE}:$TAG
+echo "tagging: $ID ${NAME}:$TAG"
+docker tag $ID ${NAME}:$TAG
 
 echo "push: $1"
 if [ "$1" == "push" ]; then
-  docker push $IMAGE
+  docker push $IMAGE:$TAG
 else
   echo "pushing disabled!"
 fi
